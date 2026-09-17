c0c042885f57bb1df04656be79cbd9bbfd419dda
joguis <jose0504sc@gmail.com>
2026-09-17 11:36:33 -0500

    Primer commit: subida micros eureka, api gateway y login jwt

---
+++ directorio/directorio/src/test/java/co/unicauca/edu/eureka/directorio/DirectorioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.edu.eureka.directorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
