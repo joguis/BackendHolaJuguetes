9a11ace01099b88dadf2bdb030b476185d07f5d9
joguis <jose0504sc@gmail.com>
2026-09-21 14:39:39 -0500

    Micro añadido a eureka y api gateway

--- inventario/src/main/java/co/holajuguetes/inventario/InventarioApplication.java
+++ inventario/src/main/java/co/holajuguetes/inventario/InventarioApplication.java
@@ -2,8 +2,10 @@ package co.holajuguetes.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventarioApplication {
 
 	public static void main(String[] args) {
